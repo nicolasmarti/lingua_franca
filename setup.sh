f10b5782c2282fabf20e3644cd521489d822cd99
Nicolas Marti <nicolas.marti.japon@gmail.com>
2026-06-07 17:01:28 +0900

    fixes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # Python
 python -m venv .venv
 source .venv/bin/activate
-pip install llvmlite maturin_import_hook rustimport juliacall
+pip install llvmlite maturin maturin_import_hook rustimport juliacall
 
 
 # Ocaml
 opam switch create . 4.14.2
 eval $(opam env)
-opam install -y ocaml-in-python llvm maturin
+opam install -y ocaml-in-python llvm
 
 #
 pip install ./_opam/lib/ocaml-in-python
